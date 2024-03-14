cfe523579b3389dd1ceb00a92c6dc774d12a3be9
Adrian Ruiz <36345673+adruri@users.noreply.github.com>
2024-03-13 21:39:51 -0600

    Adding method for printing natural numbers in ascending order

--- src/main/java/com/arr/cs/recursion/NaturalNumbers.java
+++ src/main/java/com/arr/cs/recursion/NaturalNumbers.java
@@ -6,7 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 public class NaturalNumbers {
 
     public static void main(String[] args) {
-        printNaturalNumbers(100);
+        printNaturalNumbers(10);
+        printNaturalNumbersAscending(10);
     }
 
     static void printNaturalNumbers(int n) {
@@ -15,7 +16,18 @@ public class NaturalNumbers {
         }
 
         log.info(String.valueOf(n));
+
         printNaturalNumbers(n - 1);
+    }
+
+    static void printNaturalNumbersAscending(int n) {
+        if (n <= 0) {
+            return;
+        }
+
+        printNaturalNumbersAscending(n - 1);
+
+        log.info(String.valueOf(n));
     }
 
 }
